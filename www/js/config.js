0ba2314ce98e786d066b65c8d265af7653aff24a
furneauxhugo <hugo@appdesign.company>
2015-05-28 16:40:03 +0100

    putting it in
    change

---
+++ www/js/config.js
@@ -0,0 +1,6 @@
+/* URL for the WordPress JSON API */
+var API_BASE_URL = "http://cluckinghencrafts.co.uk/";
+
+/* ID numbers for the PushWoosh messaging system */
+var PUSHWOOSH_ID = "XXXXX-XXXXX";
+var GCM_PROJECT_NUMBER = "00000000000";
